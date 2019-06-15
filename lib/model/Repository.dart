488c5f9a230b95fd355881f54d18585909846722
Felipe Athaydes <felipeathaydes@gmail.com>
2019-06-15 01:24:42 -0300

    Added the Repository property "detail"
    The returned json from gitHubs endpoint doesn`t have a "detail" field
    but it does have a "description" field.

--- lib/model/Repository.dart
+++ lib/model/Repository.dart
@@ -1,11 +1,13 @@
 class Repository {
   final String title;
+  final String detail;
 
-  Repository(this.title);
+  Repository(this.title, this.detail);
 
   factory Repository.fromJson(Map<String, dynamic> json) {
     return Repository(
       json['name'],
+      json['description'],
     );
   }
 }
