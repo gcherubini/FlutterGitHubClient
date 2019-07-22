9b960d4b9cf297a398b6b6a24e0b8ccd119127fa
Guilherme Cherubini <gabreu@thoughtworks.com>
2019-07-22 18:01:04 -0300

    Refactor: extract list view and list tile logic into new methods, make methods private

--- lib/presenter/repositories_list.dart
+++ lib/presenter/repositories_list.dart
@@ -4,50 +4,46 @@ import 'package:github_client/domain/model/Repository.dart';
 import 'package:github_client/presenter/repository_details.dart';
 
 class Repositories extends StatelessWidget {
-  Repositories({Key key, this.title}) : super(key: key);
-
-  final String title;
+  Repositories({Key key}) : super(key: key);
 
   static const routeName = '/repositoriesRoute';
+  static const _appBarTitle = 'Repositories';
 
-  @override
-  Widget build(BuildContext context) {
-    const title = 'Repositories';
-    final List<Repository> repositoryArgs =
-        ModalRoute.of(context).settings.arguments;
-    debugPrint('--> Repositories -> $repositoryArgs');
-
-    Widget buildBody(int index) {
-      return Text(repositoryArgs[index].title);
-    }
-
-    ListTile getRepositoryListItem(BuildContext context, int index) {
-      final Repository repository = repositoryArgs[index];
-
-      return ListTile(
-        onTap: () {
-          navigateToRepositoryDetails(context, repository);
-        },
-        title: Text(repository.title),
-      );
-    }
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: ListView.builder(
-          itemCount: repositoryArgs.length,
-          itemBuilder: (BuildContext context, int index) =>
-              getRepositoryListItem(context, index)),
-    );
-  }
-
-  void navigateToRepositoryDetails(BuildContext context, Repository repo) {
+  void _navigateToRepositoryDetails(BuildContext context, Repository repo) {
     Navigator.pushNamed(
       context,
       RepositoryDetails.routeName,
       arguments: repo,
+    );
+  }
+
+  ListTile _buildListTile(BuildContext context, List<Repository> repositoryArgs, int index) {
+    final Repository repository = repositoryArgs[index];
+    return ListTile(
+      onTap: () {
+        _navigateToRepositoryDetails(context, repository);
+      },
+      title: Text(repository.title),
+    );
+  }
+
+  ListView _buildListView(BuildContext context, List<Repository> repositoryArgs) {
+    return ListView.builder(
+        itemCount: repositoryArgs.length,
+        itemBuilder: (BuildContext context, int index) =>
+            _buildListTile(context, repositoryArgs, index));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Repository> repositoryArgs =
+        ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appBarTitle),
+      ),
+      body: _buildListView(context, repositoryArgs),
     );
   }
 }
